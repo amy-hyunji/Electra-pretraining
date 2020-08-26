1221d91aed98db96a24af26df5f2fb86ba293025
amy-hyunji <44370759+amy-hyunji@users.noreply.github.com>
2020-08-26 11:44:57 +0900

    enable nsml training

--- dataset/dump_openwebtext.py
+++ dataset/dump_openwebtext.py
@@ -2,26 +2,39 @@ import tarfile
 import os
 from tqdm import tqdm, notebook
 
-path = "./openwebtext/"
+path = "/Users/user/Desktop/lee/electra/dataset/openwebtext"
 txtpath = "./owt_txt/"
 savepath = "./n_owt.txt"
 
-xzFiles = os.listdir(path)
-with open(savepath, "w") as owt: 
+def extract_one():
+	xzFiles = os.listdir(path)
+	with open(savepath, "w") as owt: 
+		for i in tqdm(range(len(xzFiles))):
+			xzfile = xzFiles[i]
+			with tarfile.open(os.path.join(path, xzfile)) as f:
+				if not os.path.exists(txtpath):
+					os.mkdir(txtpath)
+				f.extractall(txtpath)
+			txtList = os.listdir(txtpath)
+			for txt in txtList:
+				temp = open(os.path.join(txtpath, txt))
+				try:
+					owt.write(temp.read())
+				except:
+					lines = temp.readlines()
+					for line in lines:
+						owt.write(line)
+			os.system(f"rm -rf {txtpath}")
+			
+def extract_all():
+	xzFiles = os.listdir(path)
+	if not os.path.exists(txtpath):
+		os.mkdir(txtpath)
 	for i in tqdm(range(len(xzFiles))):
 		xzfile = xzFiles[i]
 		with tarfile.open(os.path.join(path, xzfile)) as f:
-			if not os.path.exists(txtpath):
-				os.mkdir(txtpath)
 			f.extractall(txtpath)
-		txtList = os.listdir(txtpath)
-		for txt in txtList:
-			temp = open(os.path.join(txtpath, txt))
-			try:
-				owt.write(temp.read())
-			except:
-				lines = temp.readlines()
-				for line in lines:
-					owt.write(line)
-		os.system(f"rm -rf {txtpath}")
-		
+
+
+if __name__ == "__main__":
+	extract_all()
